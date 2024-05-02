518d0667281db2b09d813cfdc47e4eeea98639aa
lukeskytalker9 <jackebiggins@gmail.com>
2024-05-02 15:16:19 -0400

    yes

--- Ball.py
+++ Ball.py
@@ -1,7 +1,13 @@
+import numpy as np
 class Ball:
 
-    def __init__(self, x, y, radius, isPocketed=False):
-        self.x = x
-        self.y = y
+    def __init__(self, pos_x, pos_y, vel_x , vel_y , radius, isPocketed=False):
+        self.pos = np.arary([pos_x, pos_y])
+        self.vel = np.array([vel_x, vel_y])
         self.radius = radius
         self.isPocketed = isPocketed
+
+
+    def update(self, x, y):
+        self.x = x
+        self.y = y
