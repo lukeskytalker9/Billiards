ddc938c9dc2e83afed5c593f15b3f665857ccea0
Will Barr <willlovestoski@gmail.com>
2024-05-02 15:21:25 -0400

    added overlap method to state

--- State.py
+++ State.py
@@ -6,3 +6,15 @@ class State:
 
     def get_next(self, timestep):
         pass
+
+    def has_overlap(self):
+        for ball in self.balls:
+            for other in self.balls:
+                # if they're not the same ball...
+                if id(ball) != id(other):
+                    # ...and if they overlap...
+                    if ball.overlaps(other):
+                        # return true
+                        return True
+        # ... if there's no overlap, return false.
+        return False
